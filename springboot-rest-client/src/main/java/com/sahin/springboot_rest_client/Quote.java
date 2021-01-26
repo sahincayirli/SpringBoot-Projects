1a430e698fb40578ebc541d0264d206be2a060be
sahincayirli <62733795+sahincayirli@users.noreply.github.com>
2021-01-26 18:42:49 +0300

    SpringBoot + Spring Rest Template

---
+++ springboot-rest-client/src/main/java/com/sahin/springboot_rest_client/Quote.java
@@ -0,0 +1,15 @@
+package com.sahin.springboot_rest_client;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+    private String type;
+    private Value value;
+}
